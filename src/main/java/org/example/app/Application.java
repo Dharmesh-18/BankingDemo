be43e1314dea2201c3d48c9a225c716540f2e838
Dharmesh18 <dharmesh.tiwari@credable.in>
2022-02-21 09:50:45 +0530

    first commit..

---
+++ src/main/java/org/example/app/Application.java
@@ -0,0 +1,20 @@
+package org.example.app;
+
+import org.example.model.Customer;
+import org.example.service.DefaultCustomerService;
+
+import java.util.List;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        DefaultCustomerService service = new DefaultCustomerService();
+
+        List<Customer> customers = service.getAllCustomersWithStars();
+
+        customers.forEach(System.out::println);
+
+    }
+}
+
---
+++ src/main/java/org/example/repository/CustomerRepositoryDB.java
@@ -0,0 +1,35 @@
+package org.example.repository;
+
+import org.example.model.Customer;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerRepositoryDB {
+
+    public List<Customer> findAll(){
+
+        List<Customer> customers = new ArrayList<>();
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/banking?user=root&password=student");
+            PreparedStatement ps = connection.prepareStatement("Select customer_id, name, city from customers");
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                Customer customer = new Customer(
+                        rs.getString("customer_id"),
+                        rs.getString("name"),
+                        rs.getString("city")
+                );
+                customers.add(customer);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return customers;
+    }
+}
