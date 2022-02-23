9cc0c2666c1f096a7780929cc972dca05c47e008
Dharmesh18 <dharmesh.tiwari@credable.in>
2022-02-23 12:50:09 +0530

    using @Entity, @Column , application.properties, Extending JPARepository and data-jpa dependency ..made code more concise

--- src/main/java/org/example/service/CustomerService.java
+++ src/main/java/org/example/service/CustomerService.java
@@ -7,6 +7,5 @@ import java.util.List;
 public interface CustomerService {
 
     public List<Customer> getAllCustomers();
-    public List<Customer> getAllCustomersWithStars();
 
 }
--- src/main/java/org/example/service/DefaultCustomerService.java
+++ src/main/java/org/example/service/DefaultCustomerService.java
@@ -2,9 +2,7 @@ package org.example.service;
 
 import org.example.model.Customer;
 import org.example.repository.CustomerRepository;
-import org.example.repository.CustomerRepositoryDB;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,12 +28,5 @@ public class DefaultCustomerService implements CustomerService{
         return repository.findAll();
     }
 
-    public List<Customer> getAllCustomersWithStars() {
-        return repository.findAll()
-                .stream()
-                .map(c -> new Customer(c.getId(), c.getName(), c.getCity()))
-                .collect(Collectors.toList());
-
-    }
 
 }
