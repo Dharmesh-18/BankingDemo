51a25cab5e9f442cce6d548b413753142af221e8
Dharmesh18 <dharmesh.tiwari@credable.in>
2022-02-25 10:01:36 +0530

    using ResponseEntity to wrap output with HttpStatus code for the operations being performed

--- src/main/java/org/example/service/DefaultCustomerService.java
+++ src/main/java/org/example/service/DefaultCustomerService.java
@@ -3,6 +3,8 @@ package org.example.service;
 import org.example.model.Customer;
 import org.example.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,9 +31,13 @@ public class DefaultCustomerService implements CustomerService{
         return repository.findAll();
     }
 
-    public Customer getCustomer(Long id) {
+    public ResponseEntity<Customer> getCustomer(Long id) {
         Optional <Customer> optionalCustomer = repository.findById(id);
-        return optionalCustomer.get();
+        if(optionalCustomer.isPresent()){
+            return new ResponseEntity<>(optionalCustomer.get(), HttpStatus.OK );
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
     }
 
     @Override
